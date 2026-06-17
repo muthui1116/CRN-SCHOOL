1390755183f5235b0fb289b1acd3618f3be86160
muthui1116 <muthuikyalozis@gmail.com>
2026-06-17 23:07:52 +0300

    register and login margin top complete

--- db.js
+++ db.js
@@ -5,13 +5,13 @@ dotenv.config();
 
 // Create a pg db using DATABASE_URL
 const db = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    // connectionString: process.env.DATABASE_URL,
-    // ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 // Test the database connection
